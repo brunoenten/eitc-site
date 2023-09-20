5efd7d0f772d8284f762c72e9867c8307aa69d9c
Maxime Lerasle <maxlerasle@gmail.com>
2023-09-20 12:09:16 +0200

    Sort blog articles

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -17,6 +17,7 @@ export const metadata = {
 
 export default async function Blog() {
   let articles = await loadArticles()
+  console.log({ articles })
 
   return (
     <>
@@ -26,55 +27,61 @@ export default async function Blog() {
 
       <Container className="mt-24 sm:mt-32 lg:mt-40">
         <div className="space-y-24 lg:space-y-32">
-          {articles.map((article) => (
-            <FadeIn key={article.href}>
-              <article>
-                <Border className="pt-16">
-                  <div className="relative lg:-mx-4 lg:flex lg:justify-end">
-                    <div className="pt-10 lg:w-2/3 lg:flex-none lg:px-4 lg:pt-0">
-                      <h2 className="text-abbey-950 font-display text-2xl font-semibold">
-                        <Link href={article.href}>{article.title}</Link>
-                      </h2>
-                      <dl className="lg:absolute lg:left-0 lg:top-0 lg:w-1/3 lg:px-4">
-                        <dt className="sr-only">Published</dt>
-                        <dd className="text-abbey-950 absolute left-0 top-0 text-sm lg:static">
-                          <time dateTime={article.date}>
-                            {formatDate(article.date)}
-                          </time>
-                        </dd>
-                        <dt className="sr-only">Author</dt>
-                        <dd className="mt-6 flex gap-x-4">
-                          <div className="bg-abbey-100 flex-none overflow-hidden rounded-xl">
-                            <Image
-                              alt=""
-                              {...article.author.image}
-                              className="h-12 w-12 object-cover grayscale"
-                            />
-                          </div>
-                          <div className="text-abbey-950 text-sm">
-                            <div className="font-semibold">
-                              {article.author.name}
+          {articles
+            .sort((a, b) => {
+              const da = new Date(a.date)
+              const db = new Date(b.date)
+              return db - da
+            })
+            .map((article) => (
+              <FadeIn key={article.href}>
+                <article>
+                  <Border className="pt-16">
+                    <div className="relative lg:-mx-4 lg:flex lg:justify-end">
+                      <div className="pt-10 lg:w-2/3 lg:flex-none lg:px-4 lg:pt-0">
+                        <h2 className="font-display text-2xl font-semibold text-abbey-950">
+                          <Link href={article.href}>{article.title}</Link>
+                        </h2>
+                        <dl className="lg:absolute lg:left-0 lg:top-0 lg:w-1/3 lg:px-4">
+                          <dt className="sr-only">Published</dt>
+                          <dd className="absolute left-0 top-0 text-sm text-abbey-950 lg:static">
+                            <time dateTime={article.date}>
+                              {formatDate(article.date)}
+                            </time>
+                          </dd>
+                          <dt className="sr-only">Author</dt>
+                          <dd className="mt-6 flex gap-x-4">
+                            <div className="flex-none overflow-hidden rounded-xl bg-abbey-100">
+                              <Image
+                                alt=""
+                                {...article.author.image}
+                                className="h-12 w-12 object-cover grayscale"
+                              />
                             </div>
-                            <div>{article.author.role}</div>
-                          </div>
-                        </dd>
-                      </dl>
-                      <p className="text-abbey-600 mt-6 max-w-2xl text-base">
-                        {article.description}
-                      </p>
-                      <Button
-                        href={article.href}
-                        aria-label={`Read more: ${article.title}`}
-                        className="mt-8"
-                      >
-                        Read more
-                      </Button>
+                            <div className="text-sm text-abbey-950">
+                              <div className="font-semibold">
+                                {article.author.name}
+                              </div>
+                              <div>{article.author.role}</div>
+                            </div>
+                          </dd>
+                        </dl>
+                        <p className="mt-6 max-w-2xl text-base text-abbey-600">
+                          {article.description}
+                        </p>
+                        <Button
+                          href={article.href}
+                          aria-label={`Read more: ${article.title}`}
+                          className="mt-8"
+                        >
+                          Read more
+                        </Button>
+                      </div>
                     </div>
-                  </div>
-                </Border>
-              </article>
-            </FadeIn>
-          ))}
+                  </Border>
+                </article>
+              </FadeIn>
+            ))}
         </div>
       </Container>
 
